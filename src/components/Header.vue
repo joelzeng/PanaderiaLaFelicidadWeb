cf147ae3118b7a10dc2aa7d729039b3924345de9
Joel Zeng <joelzeng@hotmail.com>
2021-04-03 19:30:34 -0400

    add vuetify project setup

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar app color="primary">
+    <div class="d-flex align-center">LOGO</div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
